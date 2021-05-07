d5d28b3bf82c3ee01b38dea3cac49f399dbe9ba7
bennycio <benny@bennyc.io>
2021-05-06 20:06:00 -0700

    Symlinks

---
+++ scripts/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm ~/.config/i3/config
+rm ~/.config/i3/i3blocks.conf
+rm ~/.zshrc
+rm ~/.zshrc.pre-oh-my-zsh
+rm ~/.fehbg
+
+
+ln -s ~/.dotfiles/i3/config ~/.config/i3/config
+ln -s ~/.dotfiles/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
+ln -s ~/.dotfiles/.fehbg ~/.fehbg
